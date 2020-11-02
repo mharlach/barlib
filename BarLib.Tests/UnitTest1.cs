7468de2444d7a0bf72bdf6a0991b9a9b485e2eff
Mike Harlach <mharlach@kespry.com>
2020-11-02 14:31:45 -0500

    first commit

---
+++ BarLib.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using BarLib;
+
+namespace BarLib.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
